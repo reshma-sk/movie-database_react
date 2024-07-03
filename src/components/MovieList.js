5940156d697f3e07c4bcfb07616e021a04620cd7
Reshma <reshmashaik0910@gmail.com>
2024-07-03 16:25:05 +0100

    changed manifest file

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -6,12 +6,9 @@ const MovieList = (props) => {
 	return (
 		<>
 			{props.movies.map((movie, index) => (
-				<div className='image-container d-flex justify-content-start m-3'>
+				<div key={index} className='image-container d-flex justify-content-start m-3'>
 					<img src={movie.Poster} alt='movie'></img>
-					<div
-						onClick={() => props.handleFavouritesClick(movie)}
-						className='overlay d-flex align-items-center justify-content-center'
-					>
+					<div onClick={() => props.handleFavouritesClick(movie)} className='overlay d-flex align-items-center justify-content-center'>
 						<FavouriteComponent />
 					</div>
 				</div>
